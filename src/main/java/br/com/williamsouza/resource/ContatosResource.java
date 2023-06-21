6ec3a49b7e3c6f3f37a5075bf0a6878786fa4dc1
William de Souza <william.sza@outlook.com>
2023-06-21 14:13:58 -0300

    Fist commit

---
+++ src/main/java/br/com/williamsouza/resource/ContatosResource.java
@@ -0,0 +1,10 @@
+package br.com.williamsouza.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/contatos")
+public class ContatosResource {
+    
+}
---
+++ src/test/java/br/com/williamsouza/AgendaContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.williamsouza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
